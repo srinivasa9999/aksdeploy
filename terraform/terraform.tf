50dc70558c2c8ab19c9c6abb59046f2df4ab64c2
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-05 01:47:05 +0530

    main

---
+++ terraform/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    octopusdeploy = {
+      source = "OctopusDeployLabs/octopusdeploy"
+      version = "0.7.68"
+    }
+  }
+}
+
+provider "octopusdeploy" {
+  # Configuration options
+  address    = "https://srinivas.octopus.app/"   
+  api_key    = "API-IUDLNTKGAKKJYU2A4PVVIX5L9LXR72WA"             
+  space_id   = data.octopusdeploy_space.space.id
+}
