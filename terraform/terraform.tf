8bd0356dd74d3e9ba8cbb97446d0cb7afb9fcc06
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-05 02:55:17 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,5 +27,5 @@ data "octopusdeploy_projects" "example" {
 }
 
 output "projects" {
-    value = data.octopusdeploy_projects.example.[projects.slug]
+    value = data.octopusdeploy_projects.example.slug
 }
