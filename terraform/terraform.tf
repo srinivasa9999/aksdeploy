d0a8be72984b92e8e5228dface36c5b55686a990
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-05 02:46:19 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -23,13 +23,7 @@ output "users" {
 }
 
 data "octopusdeploy_projects" "example" {
-  cloned_from_project_id = "Projects-456"
-  ids                    = ["Projects-123", "Projects-321"]
-  is_clone               = true
-  name                   = "Default"
-  partial_name           = "Defau"
-  skip                   = 5
-  take                   = 100
+  take                   = 2
 }
 
 output "projects" {
