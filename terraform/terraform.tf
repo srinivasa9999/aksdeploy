fba0e5312a28baf0f62fb5dc2eefeb3a608338e4
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-08 00:10:08 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -20,9 +20,6 @@ resource "octopusdeploy_project_group" "gcreate" {
 
 data "octopusdeploy_project_groups" "groups" {
   partial_name  = var.pgname
-      depends_on = [
-      resource.octopusdeploy_project_group.gcreate.result
-      ]
 }
 # output "groups" {
 #     value = data.octopusdeploy_project_groups.groups.project_groups[0].id
