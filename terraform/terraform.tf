6e172689b322bcee899494f6ac34f3fbe1879733
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-07 19:17:39 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -21,9 +21,7 @@ resource "octopusdeploy_project_group" "gcreate" {
 
 data "octopusdeploy_project_groups" "groups" {
   take                   = 10
-  project_groups         = { 
-    "name" = "AVA"
-  }
+
 }
 output "groups" {
     value = data.octopusdeploy_project_groups.groups
