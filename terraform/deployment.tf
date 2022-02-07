4e20ea2d72d642d8570621e5ea5822ea90aa1df2
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-07 17:25:23 +0530

    test

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -2,24 +2,23 @@
 resource "octopusdeploy_deployment_process" "example" {
   project_id = "Projects-4"
   step {
-    condition           = "Success"
-    name                = "Hello world (using PowerShell)"
-    package_requirement = "LetOctopusDecide"
-    start_trigger       = "StartAfterPrevious"
-    run_script_action {
-      can_be_used_for_project_versioning = false
-      condition                          = "Success"
+    condition    = "Success"
+    name         = "Manual intervention"
+    manual_intervention_action {
+      name                               = "Manual intervention"
       is_disabled                        = false
       is_required                        = true
-      name                               = "Hello world ( PowerShell)"
-      script_body                        = <<-EOT
-          Write-Host 'Hello world, using PowerShell'
-          #TODO: Experiment with steps of your own :)
-          Write-Host '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
-        EOT
-      run_on_server                      = true
+      responsible_teams                  = "teams-everyone"
+      instructions                       = "Approve"
+#      can_be_used_for_project_versioning = "true"
+      properties                         = {
+            "Octopus.Action.Manual.BlockConcurrentDeployments" = "True"
+            "Octopus.Action.Manual.Instructions"              = "Approve"
+                }
+
     }
   }
+
   step {
     condition           = "Success"
     name                = "Hello world ( Bash)"
@@ -37,23 +36,6 @@ resource "octopusdeploy_deployment_process" "example" {
           echo '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
         EOT
       run_on_server                      = true
-    }
-  }
-  step {
-    condition    = "Success"
-    name         = "Manual intervention"
-    manual_intervention_action {
-      name                               = "Manual intervention"
-      is_disabled                        = false
-      is_required                        = true
-      responsible_teams                  = "teams-everyone"
-      instructions                       = "Approve"
-#      can_be_used_for_project_versioning = "true"
-      properties                         = {
-            "Octopus.Action.Manual.BlockConcurrentDeployments" = "True"
-            "Octopus.Action.Manual.Instructions"              = "Approve"
-                }
-
     }
   }
 }
