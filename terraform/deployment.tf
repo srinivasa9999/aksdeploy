653a535bc710949e80b130af2983c947701899af
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-07 16:20:06 +0530

    test

---
+++ terraform/deployment.tf
@@ -0,0 +1,62 @@
+
+resource "octopusdeploy_deployment_process" "example" {
+  project_id = "Projects-4"
+#   step {
+#     condition    = "Success"
+#     name         = "Manual intervention is needed"
+# #    instructions = "Approve"
+# #    can_be_used_for_project_versioning = "true"
+#     manual_intervention_action {
+#       name                               = "Manual intervention is needed"
+#       is_disabled                        = false
+#       is_required                        = true
+#       responsible_teams                  = "Everyone"
+#       instructions                       = "Approve"
+#       can_be_used_for_project_versioning = "true"
+# #      properties                         = "BlockConcurrentDeployments"
+#     }
+#   }
+
+
+
+
+
+  step {
+    condition           = "Success"
+    name                = "Hello world (using PowerShell)"
+    package_requirement = "LetOctopusDecide"
+    start_trigger       = "StartAfterPrevious"
+    run_script_action {
+      can_be_used_for_project_versioning = false
+      condition                          = "Success"
+      is_disabled                        = false
+      is_required                        = true
+      name                               = "Hello world (using PowerShell)"
+      script_body                        = <<-EOT
+          Write-Host 'Hello world, using PowerShell'
+          #TODO: Experiment with steps of your own :)
+          Write-Host '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
+        EOT
+      run_on_server                      = true
+    }
+  }
+  step {
+    condition           = "Success"
+    name                = "Hello world (using Bash)"
+    package_requirement = "LetOctopusDecide"
+    start_trigger       = "StartWithPrevious"
+    run_script_action {
+      can_be_used_for_project_versioning = false
+      condition                          = "Success"
+      is_disabled                        = false
+      is_required                        = true
+      name                               = "Hello world (using Bash)"
+      script_body                        = <<-EOT
+          echo 'Hello world, using Bash'
+          #TODO: Experiment with steps of your own :)
+          echo '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
+        EOT
+      run_on_server                      = true
+    }
+  }
+}
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,7 +27,7 @@ resource "octopusdeploy_project" "pcreate" {
   discrete_channel_release             = false
   is_disabled                          = false
   is_discrete_channel_release          = false
-  is_version_controlled                = true
+  is_version_controlled                = false
   lifecycle_id                         = "Lifecycles-2"   #variable
   name                                 = "AVAReport"      #variable
   project_group_id                     = "ProjectGroups-2" #variable
@@ -67,65 +67,7 @@ output "groups" {
     value = data.octopusdeploy_project_groups.groups
 }
 
-
-resource "octopusdeploy_deployment_process" "example" {
-  project_id = "Projects-4"
-#   step {
-#     condition    = "Success"
-#     name         = "Manual intervention is needed"
-# #    instructions = "Approve"
-# #    can_be_used_for_project_versioning = "true"
-#     manual_intervention_action {
-#       name                               = "Manual intervention is needed"
-#       is_disabled                        = false
-#       is_required                        = true
-#       responsible_teams                  = "Everyone"
-#       instructions                       = "Approve"
-#       can_be_used_for_project_versioning = "true"
-# #      properties                         = "BlockConcurrentDeployments"
-#     }
-#   }
-
-
-
-
-
-  step {
-    condition           = "Success"
-    name                = "Hello world (using PowerShell)"
-    package_requirement = "LetOctopusDecide"
-    start_trigger       = "StartAfterPrevious"
-    run_script_action {
-      can_be_used_for_project_versioning = false
-      condition                          = "Success"
-      is_disabled                        = false
-      is_required                        = true
-      name                               = "Hello world (using PowerShell)"
-      script_body                        = <<-EOT
-          Write-Host 'Hello world, using PowerShell'
-          #TODO: Experiment with steps of your own :)
-          Write-Host '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
-        EOT
-      run_on_server                      = true
-    }
-  }
-  step {
-    condition           = "Success"
-    name                = "Hello world (using Bash)"
-    package_requirement = "LetOctopusDecide"
-    start_trigger       = "StartWithPrevious"
-    run_script_action {
-      can_be_used_for_project_versioning = false
-      condition                          = "Success"
-      is_disabled                        = false
-      is_required                        = true
-      name                               = "Hello world (using Bash)"
-      script_body                        = <<-EOT
-          echo 'Hello world, using Bash'
-          #TODO: Experiment with steps of your own :)
-          echo '[Learn more about the types of steps available in Octopus](https://g.octopushq.com/OnboardingAddStepsLearnMore)'
-        EOT
-      run_on_server                      = true
-    }
-  }
+module "deployment" {
+  source = "./deployment.tf"
+  
 }
