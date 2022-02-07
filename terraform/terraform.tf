d68219530ca36926b6269723d10009bfc863b77b
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-07 22:15:21 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -22,50 +22,50 @@ data "octopusdeploy_project_groups" "groups" {
   partial_name  = var.pgname
 }
 output "groups" {
-    value = data.octopusdeploy_project_groups.groups.project_groups[1].id
+    value = data.octopusdeploy_project_groups.groups.project_groups[0]].id
 }
 
 
 
-# resource "octopusdeploy_project" "pcreate" {
-#   space_id                             =  "Spaces-1"
-#   auto_create_release                  = false
-#   default_guided_failure_mode          = "EnvironmentDefault"
-#   default_to_skip_if_already_installed = false
-#   description                          = "The development project."
-#   discrete_channel_release             = false
-#   is_disabled                          = false
-#   is_discrete_channel_release          = false
-#   is_version_controlled                = false
-#   lifecycle_id                         = "Lifecycles-2"   #variable
-#   name                                 = var.pname      #variable
-#   project_group_id                     = data.octopusdeploy_project_groups.groups.project_groups[*].id
-#   tenanted_deployment_participation    = "Untenanted"
+resource "octopusdeploy_project" "pcreate" {
+  space_id                             =  "Spaces-1"
+  auto_create_release                  = false
+  default_guided_failure_mode          = "EnvironmentDefault"
+  default_to_skip_if_already_installed = false
+  description                          = "The development project."
+  discrete_channel_release             = false
+  is_disabled                          = false
+  is_discrete_channel_release          = false
+  is_version_controlled                = false
+  lifecycle_id                         = "Lifecycles-2"   #variable
+  name                                 = var.pname      #variable
+  project_group_id                     = data.octopusdeploy_project_groups.groups.project_groups[0].id
+  tenanted_deployment_participation    = "Untenanted"
 
 
   
-#   connectivity_policy {
-#     allow_deployments_to_no_targets = false
-#     exclude_unhealthy_targets       = false
-#     skip_machine_behavior           = "None"
+  connectivity_policy {
+    allow_deployments_to_no_targets = false
+    exclude_unhealthy_targets       = false
+    skip_machine_behavior           = "None"
+  }
+#   git_persistence_settings  "git" {
+#     url  = "https://github.com/srinivasa9999/aksdeploy.git"
 #   }
-# #   git_persistence_settings  "git" {
-# #     url  = "https://github.com/srinivasa9999/aksdeploy.git"
-# #   }
-# #   git_persistence_settings.credentials {
-# #     username = "srinivasa9999"
-# #     password = "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"
-# #   }
+#   git_persistence_settings.credentials {
+#     username = "srinivasa9999"
+#     password = "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"
+#   }
 
-#   template {
-#     default_value = "example-default-value"
-#     help_text     = "example-help-test"
-#     label         = "example-label"
-#     name          = "example-template-value"
-#     display_settings = {
-#       "Octopus.ControlType" : "SingleLineText"
-#     }
-#   }
-# }
+  template {
+    default_value = "example-default-value"
+    help_text     = "example-help-test"
+    label         = "example-label"
+    name          = "example-template-value"
+    display_settings = {
+      "Octopus.ControlType" : "SingleLineText"
+    }
+  }
+}
 
 
