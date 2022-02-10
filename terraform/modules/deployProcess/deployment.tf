556f862ea69601e4df7cb023ff3d9147b6a33d8f
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-11 00:21:24 +0530

    test

--- terraform/modules/deployProcess/deployment.tf
+++ terraform/modules/deployProcess/deployment.tf
@@ -49,7 +49,37 @@ resource "octopusdeploy_deployment_process" "example" {
     }
   }
 
+  step {
+    condition           = "Success"
+    name                = "Kubernetes Deploy"
+    package_requirement = "LetOctopusDecide"
+    start_trigger       = "StartAfterPrevious"
+    target_roles        = ["test"]
+    run_kubectl_script_action {
+      can_be_used_for_project_versioning = false
+      condition                          = "Success"
+      environments                       = []
+      excluded_environments              = []
+      name                               = "Run a kubectl CLI Script"
+      is_disabled                        = false
+      is_required                        = true
+      properties                         = {
+          "Octopus.Action.KubernetesContainers.Namespace" = "default"
+          "Octopus.Action.RunOnServer"                    = "true"
+          "Octopus.Action.Script.ScriptBody"              = "kubectl apply deploy.yml"
+          "Octopus.Action.Script.ScriptSource"            = "Inline"
+          "Octopus.Action.Script.Syntax"                  = "Bash"
+      }
+      run_on_server                      = "true"
+      worker_pool_id                     = "WorkerPools-5"
+    }
+  }
+
+
+
   # depends_on = [
   #    octopusdeploy_project.pcreate
   #      ]
 }
+
+
