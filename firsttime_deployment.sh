1813a3d24a558dbdcc59adb6bf60af1c184ec2a3
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-14 01:38:03 +0530

    test

--- firsttime_deployment.sh
+++ firsttime_deployment.sh
@@ -2,7 +2,7 @@ JBNUMBER=`echo $(get_octopusvariable "JBNUM")`
 echo "*******jenkins build: $JBNUMBER"
 echo "***********3"
 
-sed -i s/imageversion/$JBNUMBER/g ../k8stest/deploy.yml
+sed -i s/imageversion/$JBNUMBER/g ./k8stest/deploy.yml
 
 DTYPE=`echo $(get_octopusvariable "DEPLOYTYPE")`
 echo "**********Deploy Type : $DTYPE"
