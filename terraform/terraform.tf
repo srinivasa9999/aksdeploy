4a1d81050b68857a070f15236c7f4924db8754c1
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-15 01:56:09 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -45,7 +45,7 @@ resource "octopusdeploy_project" "pcreate" {
   is_disabled                          = false
   is_discrete_channel_release          = false
   is_version_controlled                = false
-  lifecycle_id                         = "Lifecycles-2"   #variable
+  lifecycle_id                         = "Lifecycles-23"   #variable
   name                                 = var.pname      #variable
   project_group_id                     = octopusdeploy_project_group.gcreate.id
   tenanted_deployment_participation    = "Untenanted"
