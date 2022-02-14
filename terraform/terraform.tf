300d20784f993d755c57196edd6b3fd3513242ae
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-14 20:50:46 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -102,6 +102,6 @@ data "octopusdeploy_environments" "example" {
   take         = 100
 }
 output "environments" {
-    value = data.octopusdeploy_environments.example.name
+    value = data.octopusdeploy_environments.example.environments.name
 }
 
