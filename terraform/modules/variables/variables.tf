269023f9677a205d3e2b9404e26e543a87aafbfc
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-14 23:35:15 +0530

    test

---
+++ terraform/modules/variables/variables.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    octopusdeploy = {
+      source = "OctopusDeployLabs/octopusdeploy"
+      version = "0.7.64"
+    }
+  }
+}
+
+resource "octopusdeploy_variable" "deploytype" {
+     name      = "deploytype"
+     type      = "String"
+     prompt    = {
+         "is_required"  = "true"
+     }
+
+}
