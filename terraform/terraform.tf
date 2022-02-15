c1672baff18a577f7e2f41900963f0ff772bd077
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-15 18:00:16 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -36,22 +36,11 @@ resource "octopusdeploy_environment" "environments" {
   allow_dynamic_infrastructure = false
   name                         = each.value
   use_guided_failure           = false
-  # lifecycle {
-  #       ignore_changes = [name]
-  #}
 }
 
 output "envs" {
   value = values(octopusdeploy_environment.environments)[*]
 }
-
-# resource "octopusdeploy_environment" "development" {
-#   allow_dynamic_infrastructure = false
-#   description                  = "An environment for the development team."
-#   name                         = "development"
-#   sort_order                   = 0
-#   use_guided_failure           = false
-# }
 
 locals {
   vardev = octopusdeploy_environment.environments["development"].id
@@ -130,13 +119,6 @@ resource "octopusdeploy_project" "pcreate" {
     exclude_unhealthy_targets       = false
     skip_machine_behavior           = "None"
   }
-#   git_persistence_settings  "git" {
-#     url  = "https://github.com/srinivasa9999/aksdeploy.git"
-#   }
-#   git_persistence_settings.credentials {
-#     username = "srinivasa9999"
-#     password = "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"
-#   }
 
   template {
     default_value = "example-default-value"
@@ -162,36 +144,132 @@ depends_on  = [octopusdeploy_project_group.gcreate]
 
 
 
-# locals {
-#   projectid = octopusdeploy_project.pcreate.id
-# }
 
-# resource "octopusdeploy_variable" "deploytype" {
-#      name      = "deploytype"
-#      type      = "String"
-#      owner_id = local.projectid
-#      prompt     {
-#          is_required  = "true"
-#      }
-# }
-# resource "octopusdeploy_variable" "imageversion" {
-#      name      = "imageversion"
-#      type      = "String"
-#      owner_id = local.projectid
-#      prompt     {
-#          is_required  = "true"
-#  #        ControlType  = "dropdown"
-#      }
-# }
-# resource "octopusdeploy_variable" "environment" {
-#      name      = "environment"
-#      type      = "String"
-#      owner_id = local.projectid
-#      prompt     {
-#          is_required  = "true"
-#  #        ControlType  = "dropdown"
-#      }
-# }
+resource "octopusdeploy_deployment_process" "example" {
+  project_id = octopusdeploy_project.pcreate.id
+  step {
+    condition    = "Success"
+    name         = "Manual intervention"
+    manual_intervention_action {
+      name                               = "Manual intervention"
+      is_disabled                        = false
+      is_required                        = true
+      responsible_teams                  = "teams-everyone"
+      instructions                       = "Approve"
+#      can_be_used_for_project_versioning = "true"
+      properties                         = {
+            "Octopus.Action.Manual.BlockConcurrentDeployments" = "True"
+            "Octopus.Action.Manual.Instructions"              = "Approve"
+                }
+
+    }
+  }
+
+  step {
+    condition           = "Success"
+    name                = "Hello world ( Bash)"
+    package_requirement = "LetOctopusDecide"
+    start_trigger       = "StartAfterPrevious"
+    run_script_action {
+      can_be_used_for_project_versioning = false
+      condition                          = "Success"
+      is_disabled                        = false
+      is_required                        = true
+      name                               = "Hello world ( Bash)"
+      script_syntax                      = "Bash"
+      script_body                        = <<-EOT
+          echo 'Hello world, using Bash'
+          ls -ltr
+          
+        EOT
+      run_on_server                      = "true"
+      worker_pool_id                     = "WorkerPools-5"
+    }
+  }
+
+  step {
+    condition           = "Success"
+    name                = "Kubernetes Deploy"
+    start_trigger       = "StartAfterPrevious"
+    target_roles        = ["test"]
+    run_kubectl_script_action {
+      can_be_used_for_project_versioning = true
+      condition                          = "Success"
+      environments                       = []
+      excluded_environments              = []
+      name                               = "Run a kubectl CLI Script"
+      is_disabled                        = false
+      is_required                        = false
+      # properties                         = {
+      #     "Octopus.Action.KubernetesContainers.Namespace" = "default"
+      #     "Octopus.Action.RunOnServer"                    = "true"
+      #     "Octopus.Action.Script.ScriptBody"              = "kubectl apply deploy.yml"
+      #     "Octopus.Action.Script.ScriptSource"            = "Inline"
+      #     "Octopus.Action.Script.Syntax"                  = "Bash"
+      # }
+      properties                         = {
+          "Octopus.Action.Package.DownloadOnTentacle" = "True"
+          "Octopus.Action.Package.FeedId"             = "Feeds-1002"
+          "Octopus.Action.Package.PackageId"          = "srinivasa9999/aksdeploy"
+ #         "Octopus.Action.RunOnServer"                = "True"
+          "Octopus.Action.Script.ScriptFileName"      = "firsttime_deployment.sh"
+          "Octopus.Action.Script.ScriptSource"        = "Package"
+      }
+                
+#   #    script_source                      = "Inline"
+       run_on_server                      = "true"
+       script_file_name                   = "firsttime_deployment.sh"
+       package {
+          acquisition_location = "ExecutionTarget"
+          feed_id              = "Feeds-1002"
+#          id                   = "test"
+          name                 = "k8stest"
+          extract_during_deployment = "true"
+          package_id           = "srinivasa9999/k8stest"
+          # properties           = {
+          #   "Octopus.Action.RunOnServer"                = "True"
+          # }
+      }
+    }
+  }
+
+  depends_on = [
+     octopusdeploy_project.pcreate
+       ]
+}
+
+
+
+locals {
+  projectid = octopusdeploy_project.pcreate.id
+}
+
+resource "octopusdeploy_variable" "deploytype" {
+     name      = "deploytype"
+     type      = "String"
+     owner_id = local.projectid
+     prompt     {
+         is_required  = "true"
+     }
+}
+resource "octopusdeploy_variable" "imageversion" {
+     name      = "imageversion"
+     type      = "String"
+     owner_id = local.projectid
+     prompt     {
+         is_required  = "true"
+ #        ControlType  = "dropdown"
+     }
+}
+resource "octopusdeploy_variable" "environment" {
+     name      = "environment"
+     type      = "String"
+     owner_id = local.projectid
+     prompt     {
+         is_required  = "true"
+ #        ControlType  = "dropdown"
+     }
+}
 
 
 
