0ca437195b33b26fae9c3d1386e3b0e7a57a8dcf
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-16 01:45:43 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -205,6 +205,7 @@ resource "octopusdeploy_deployment_process" "deploymentProcess" {
     name                = "Kubernetes Deploy"
     start_trigger       = "StartAfterPrevious"
     target_roles        = ["Development"]
+    worker_pool_id                     = octopusdeploy_dynamic_worker_pool.dynamicworker.id
     run_kubectl_script_action {
       can_be_used_for_project_versioning = true
       condition                          = "Success"
@@ -213,7 +214,7 @@ resource "octopusdeploy_deployment_process" "deploymentProcess" {
       name                               = "Run a kubectl CLI Script"
       is_disabled                        = false
       is_required                        = false
-      worker_pool_id                     = octopusdeploy_dynamic_worker_pool.dynamicworker.id
+     # worker_pool_id                     = octopusdeploy_dynamic_worker_pool.dynamicworker.id
       # properties                         = {
       #     "Octopus.Action.KubernetesContainers.Namespace" = "default"
       #     "Octopus.Action.RunOnServer"                    = "true"
@@ -231,7 +232,7 @@ resource "octopusdeploy_deployment_process" "deploymentProcess" {
       }
                 
 #   #    script_source                      = "Inline"
-       run_on_server                      = "true"
+    #   run_on_server                      = "true"
        script_file_name                   = "firsttime_deployment.sh"
        package {
           acquisition_location = "ExecutionTarget"
