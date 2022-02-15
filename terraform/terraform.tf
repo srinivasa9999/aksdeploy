a40c8e316b58d0338a44c4ca4ae4607829f77027
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-15 19:50:44 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -274,7 +274,7 @@ resource "octopusdeploy_variable" "environment" {
 
 data "octopusdeploy_spaces" "spaces" {
   take         = 100
-  spaces       = {
+  spaces        {
     is_default  = true
 
   }
