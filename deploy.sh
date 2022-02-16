d09a0023385d37b366a8dfba9ebf30008b472568
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-16 13:48:41 +0530

    test

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,49 @@
-case $SERVICE_NAME in
+SERVICENAMES=("shoppingCart" "productMgm" "customerMgm") ##input variable
+for SERVICE_NAME in ${SERVICENAMES[@]}; do
 
-  shoppingCart)
-    if [[ $DTYPE == "firsttime" ]];then
-         kubectl apply -f ./$SERVICE_NAME -R --record
-    elif [[ $DTYPE == "updatedeploy" ]];then
-        kubectl apply -f ./$SERVICE_NAME/deploy.yml -R --record
-    elif [[ $DTYPE == "stop" ]];then
-        kubectl scale deploy $SERVICE_NAME --replicas=0
-    elif [[ $DTYPE == "start" ]];then
-        kubectl scale deploy $SERVICE_NAME --replicas=3
-    fi
-    ;;
+    case $SERVICE_NAME in
 
-  productMgm)
-    if [[ $DTYPE == "firsttime" ]];then
-         kubectl apply -f ./$SERVICE_NAME -R --record
-    elif [[ $DTYPE == "updatedeploy" ]];then
-        kubectl apply -f ./$SERVICE_NAME/deploy.yml -R --record
-    elif [[ $DTYPE == "stop" ]];then
-        kubectl scale deploy $SERVICE_NAME --replicas=0
-    elif [[ $DTYPE == "start" ]];then
-        kubectl scale deploy $SERVICE_NAME --replicas=3
-    fi
-    ;;
+    shoppingCart)
+        if [[ $DTYPE == "firsttime" ]];then
+            kubectl apply -f ./$SERVICE_NAME -R --record
+        elif [[ $DTYPE == "updatedeploy" ]];then
+            kubectl apply -f ./$SERVICE_NAME/deploy.yml -R --record
+        elif [[ $DTYPE == "stop" ]];then
+            kubectl scale deploy $SERVICE_NAME --replicas=0
+        elif [[ $DTYPE == "start" ]];then
+            kubectl scale deploy $SERVICE_NAME --replicas=3
+        fi
+        ;;
 
-  customerMgm)
-    if [[ $DTYPE == "firsttime" ]];then
-         kubectl apply -f ./$SERVICE_NAME -R --record
-    elif [[ $DTYPE == "updatedeploy" ]];then
-        kubectl apply -f ./$SERVICE_NAME/deploy.yml -R --record
-    elif [[ $DTYPE == "stop" ]];then
-        kubectl scale deploy $SERVICE_NAME --replicas=0
-    elif [[ $DTYPE == "start" ]];then
-        kubectl scale deploy $SERVICE_NAME --replicas=3
-    fi
-    ;;
+    productMgm)
+        if [[ $DTYPE == "firsttime" ]];then
+            kubectl apply -f ./$SERVICE_NAME -R --record
+        elif [[ $DTYPE == "updatedeploy" ]];then
+            kubectl apply -f ./$SERVICE_NAME/deploy.yml -R --record
+        elif [[ $DTYPE == "stop" ]];then
+            kubectl scale deploy $SERVICE_NAME --replicas=0
+        elif [[ $DTYPE == "start" ]];then
+            kubectl scale deploy $SERVICE_NAME --replicas=5
+        fi
+        ;;
 
-  *)
-    STATEMENTS
-    ;;
-esac
+    customerMgm)
+        if [[ $DTYPE == "firsttime" ]];then
+            kubectl apply -f ./$SERVICE_NAME -R --record
+        elif [[ $DTYPE == "updatedeploy" ]];then
+            kubectl apply -f ./$SERVICE_NAME/deploy.yml -R --record
+        elif [[ $DTYPE == "stop" ]];then
+            kubectl scale deploy $SERVICE_NAME --replicas=0
+        elif [[ $DTYPE == "start" ]];then
+            kubectl scale deploy $SERVICE_NAME --replicas=2
+        elif [[ $DTYPE == "restart" ]];then
+            kubectl scale deploy $SERVICE_NAME --replicas=0
+            kubectl scale deploy $SERVICE_NAME --replicas=2
+        fi
+        ;;
+
+    *)
+        echo "K8s deploy not processed"
+        ;;
+    esac
+done
