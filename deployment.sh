4e5859e05f09ce00a50055466735f440bf7a2567
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-18 02:39:36 +0530

    test

--- deployment.sh
+++ deployment.sh
@@ -1,32 +1,33 @@
 SERVICES=("shoppingCart" "productMgm")
 for SERVICE_NAME in ${SERVICES[@]};do
-    SERVICES=shoppingCart
-    DEPLOY_ACTION=firsttime
-    DEPLOY_STRATEGY=rollingupdate
-    K8S_REPO="https://github.com/srinivasa9999/shoppingCart.git"
-    git checkout $K8S_REPO -b $ENVIRONMENT $SERVICE_NAME
+    echo $SERVICE_NAME
+    DEPLOY_ACTION=$(python3 yamlparser.py $SERVICE_NAME deploy_action)
+    DEPLOY_STRATEGY=$(python3 yamlparser.py shoppingCart deploy_strategy)
+    K8S_REPO=$(python3 yamlparser.py shoppingCart git_url)
+    echo -e "$SERVICE_NAME" -> "$DEPLOY_ACTION" -> "$DEPLOY_STRATEGY" -> "$K8S_REPO"
+    # git checkout $K8S_REPO -b $ENVIRONMENT $SERVICE_NAME
 
-    case $DEPLOY_ACTION in
-        firsttime)
-            kubectl apply -f ./$SERVICE_NAME/$DEPLOY_STRATEGY -R
-            ;;
-        upgrade)
-            kubectl apply -f ./$SERVICE_NAME/$DEPLOY_STRATEGY/deploy.yml ##Version will change
-            ;;
-        stop)
-            kubectl scale deploy $DEPLOY_NAME --replicas=0
-            ;;
-        start)
-            kubectl scale deploy $DEPLOY_NAME --replicas=1
-            ;;
-        restart)
-            kubectl scale deploy $DEPLOY_NAME --replicas=0
-            kubectl scale deploy $DEPLOY_NAME --replicas=1
-            ;;
-        *)
-        echo "Service name Incorrect"
-        exit 1
-    esac
+    # case $DEPLOY_ACTION in
+    #     firsttime)
+    #         kubectl apply -f ./$SERVICE_NAME/$DEPLOY_STRATEGY -R
+    #         ;;
+    #     upgrade)
+    #         kubectl apply -f ./$SERVICE_NAME/$DEPLOY_STRATEGY/deploy.yml ##Version will change
+    #         ;;
+    #     stop)
+    #         kubectl scale deploy $DEPLOY_NAME --replicas=0
+    #         ;;
+    #     start)
+    #         kubectl scale deploy $DEPLOY_NAME --replicas=1
+    #         ;;
+    #     restart)
+    #         kubectl scale deploy $DEPLOY_NAME --replicas=0
+    #         kubectl scale deploy $DEPLOY_NAME --replicas=1
+    #         ;;
+    #     *)
+    #     echo "Service name Incorrect"
+    #     exit 1
+    # esac
 done
        
       
