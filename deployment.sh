75b5cb7e1cd595545a6fef05f2032edc5d7b2230
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-18 03:34:33 +0530

    test

--- deployment.sh
+++ deployment.sh
@@ -1,10 +1,10 @@
-SERVICES=("shoppingCart" "productMgm")
+SERVICES=("$@")
 for SERVICE_NAME in ${SERVICES[@]};do
     echo $SERVICE_NAME
     DEPLOY_ACTION=$(python3 yamlparser.py $SERVICE_NAME deploy_action)
-    DEPLOY_STRATEGY=$(python3 yamlparser.py shoppingCart deploy_strategy)
-    K8S_REPO=$(python3 yamlparser.py shoppingCart git_url)
-    ENVIRONMENT=$(python3 yamlparser.py shoppingCart environment)
+    DEPLOY_STRATEGY=$(python3 yamlparser.py $SERVICE_NAME deploy_strategy)
+    K8S_REPO=$(python3 yamlparser.py $SERVICE_NAME git_url)
+    ENVIRONMENT=$(python3 yamlparser.py $SERVICE_NAME environment)
     echo -e "$SERVICE_NAME -> $DEPLOY_ACTION -> $DEPLOY_STRATEGY -> $K8S_REPO"
 
     git clone $K8S_REPO -b $ENVIRONMENT $SERVICE_NAME
