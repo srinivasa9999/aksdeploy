ed2e8e07bb5bc33c47b178a2464f12fbf1506ee3
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-18 23:39:55 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -165,7 +165,7 @@ resource "octopusdeploy_dynamic_worker_pool" "dynamicworker" {
 resource "octopusdeploy_ssh_key_account" "sshaccount" {
   name             = "SSH Key Pair Account"
   private_key_file = "instance2.key"
-  username         = "srinivasarao_nallapati"
+  username         = "srinivas"
 }
 resource "octopusdeploy_ssh_connection_deployment_target" "vmtarget" {
   name        = "vmtarget"
