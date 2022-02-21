04778e0733fffbea3dc077563948103aeb221c04
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-21 19:03:14 +0530

    sssssssssssssss

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -201,7 +201,7 @@ locals "projectlist" {
  resource "octopusdeploy_deployment_process" "deploymentProcess" {
    for_each = toset(var.projectlists)
    count                   = length(var.pname)
-  project_id              =  octopusdeploy_project.pcreate[count.index]
+  project_id              =  octopusdeploy_project.pcreate[each.id]
   step {
     condition           = "Success"
     name                = "Deployment Summary"
