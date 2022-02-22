7c15ea368ed5af5eb43f20e58ddd563372bd2c33
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-23 02:54:48 +0530

    test

--- yamlparser.py
+++ yamlparser.py
@@ -3,4 +3,6 @@ import sys
 
 with open("vars.yaml", 'r') as f:
     valuesYaml = yaml.load(f, Loader=yaml.FullLoader)
-    print(valuesYaml[sys.argv[1]][sys.argv[2]])
+
+
+print(valuesYaml[sys.argv[1]][sys.argv[2]])
