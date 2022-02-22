bc90c6928a20c56cfbfd81d91cea0fd27f901f94
srinivasa9999 <srinivasa.nallapati@gmail.com>
2022-02-22 11:32:27 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -171,8 +171,13 @@ resource "octopusdeploy_kubernetes_cluster_deployment_target" "k8s-target" {
 resource "octopusdeploy_dynamic_worker_pool" "dynamicworker" {
     name                          = "dynamicWorkerpool"
     worker_type                   = "UbuntuDefault"
-    is_default                    = "true"
+    is_default                    = "false"
     description                   =  "workers will be loaded from Octopus cloud"
+}
+
+resource "octopusdeploy_static_worker_pool" "staticworker" {
+    name                          = "StaticWorkerpool"
+    is_default                    = "false"
 }
 
 # resource "octopusdeploy_ssh_key_account" "sshaccount" {
